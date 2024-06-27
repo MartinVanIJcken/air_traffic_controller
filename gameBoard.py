19e7ee9f9883038dfbf323d70956ad65f734b4d6
MartinVanIJcken <daanvanijcken@hotmail.com>
2024-06-27 17:18:01 +0200

    fixed typo

--- gameBoard.py
+++ gameBoard.py
@@ -1,118 +1,60 @@
+from __future__ import annotations
 from dataclasses import dataclass
 import unittest
 import itertools
 import numpy as np
 
-
-
-class TestPathMethods(unittest.TestCase):
-    def test_path_from_points(self):
-        path = Path.from_points([(1,1), (3,1), (3,2), (1,2)])
-        self.assertEqual(path.locations, [(1, 1), (2, 1), (3, 1), (3, 2), (2, 2), (1, 2)])
-        
-    def test_good_path_gets_accepted(self):
-        path = Path.from_points([(1,1), (3,1), (3,2), (1,2)])
-        filling = Filling([EMPTY, NORTH_FACING_PLANE, EMPTY, EMPTY, SOUTH_FACING_PLANE])
-        self.assertTrue(path.verify_filling(filling))
-
-    def test_plane_not_along_path(self):
-        path = Path.from_points([(1,1), (3,1), (3,2)])
-        filling = Filling([EMPTY, NORTH_FACING_PLANE, EMPTY, NORTH_FACING_PLANE])
-        with self.assertRaises(PlaneDirectionError):
-            path.verify_filling(filling)
-            
-    def test_planes_in_different_directions(self):
-        path = Path.from_points([(1,1), (3,1), (3,2)])
-        filling = Filling([EMPTY, NORTH_FACING_PLANE, EMPTY, WEST_FACING_PLANE])
-        with self.assertRaises(PlaneDirectionError):
-            path.verify_filling(filling)
-
-    def test_plane_on_corner(self):
-        path = Path.from_points([(1,1), (3,1), (3,2)])
-        filling = Filling([EMPTY, NORTH_FACING_PLANE, NORTH_FACING_PLANE, EMPTY])
-        with self.assertRaises(PlaneLocationError):
-            path.verify_filling(filling)
-
-    def test_plane_at_start_and_end(self):
-        path = Path.from_points([(1,1), (1,4), (3,4)])
-        filling = Filling([WEST_FACING_PLANE, EMPTY, EMPTY, EMPTY, EMPTY, SOUTH_FACING_PLANE])
-        self.assertTrue(path.verify_filling(filling))
-        
-    def test_bad_plane_at_end(self):
-        path = Path.from_points([(1,1), (1,4), (3,4)])
-        filling = Filling([EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, WEST_FACING_PLANE])
-        with self.assertRaises(PlaneDirectionError):
-            self.assertTrue(path.verify_filling(filling))
-
-    def test_bad_plane_at_start(self):
-        path = Path.from_points([(1,1), (1,4), (3,4)])
-        filling = Filling([SOUTH_FACING_PLANE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY])
-        with self.assertRaises(PlaneDirectionError):
-            self.assertTrue(path.verify_filling(filling))
-        
-    def test_directed_path_followed(self):
-        path = Path.from_points([(1,1), (1,4), (3,4)], flying_forward_mandatory=True)
-        filling = Filling([WEST_FACING_PLANE, EMPTY, EMPTY, EMPTY, EMPTY, SOUTH_FACING_PLANE])
-        self.assertTrue(path.verify_filling(filling))
-
-    def test_directed_path_not_followed(self):
-        path = Path.from_points([(3,4), (1,4), (1,1)], flying_forward_mandatory=True)
-        filling = Filling([WEST_FACING_PLANE, EMPTY, EMPTY, EMPTY, EMPTY, SOUTH_FACING_PLANE])
-        with self.assertRaises(PlaneDirectionError):
-            path.verify_filling(filling)
-    
-    def test_mandatory_planes_present(self):
-        path = Path.from_points([(50, 43), (50, 50)], mandatory_planes=[0,1,2])
-        filling = Filling([WEST_FACING_PLANE, WEST_FACING_PLANE, WEST_FACING_PLANE, EMPTY, EMPTY, WEST_FACING_PLANE, EMPTY, EMPTY])
-        self.assertTrue(path.verify_filling(filling))
-
-    def test_mandatory_plane_missing(self):
-        path = Path.from_points([(50, 43), (50, 50)], mandatory_planes=[0,1,2])
-        filling = Filling([WEST_FACING_PLANE, EMPTY, WEST_FACING_PLANE, EMPTY, EMPTY, WEST_FACING_PLANE, EMPTY, EMPTY])
-        with self.assertRaises(MissingPlaneError):
-            path.verify_filling(filling)
-                
-
-class MissingPlaneError(Exception):
-    pass
-
-class PlaneDirectionError(Exception):
-    pass
-
-class PlaneLocationError(Exception):
+class InvalidFillingException(Exception):
     pass
 
 
+class MissingPlaneException(InvalidFillingException):
+    pass
 
-class CompassDirection:
+
+class PlaneDirectionException(InvalidFillingException):
+    pass
+
+
+class PlaneLocationException(InvalidFillingException):
+    pass
+
+
+class CardinalDirection:
     __match_args__ = tuple("direction")
+
     def __init__(self, direction):
+        if direction not in "NORTH SOUTH WEST EAST".split():
+            raise ValueError(f"{direction} is not a cardinal direction.")
         self.direction = direction
-    
-    
+
     def opposite_direction(self):
         if self.direction == "NORTH":
-            return CompassDirection("SOUTH")
+            return CardinalDirection("SOUTH")
         elif self.direction == "WEST":
-            return CompassDirection("EAST")
+            return CardinalDirection("EAST")
         elif self.direction == "SOUTH":
-            return CompassDirection("NORTH")
+            return CardinalDirection("NORTH")
         elif self.direction == "EAST":
-            return CompassDirection("WEST")
+            return CardinalDirection("WEST")
 
     def __eq__(self, other):
         return self.direction == other.direction
 
-NORTH = CompassDirection("NORTH")
-WEST = CompassDirection("WEST")
-SOUTH = CompassDirection("SOUTH")
-EAST = CompassDirection("EAST")
+    def __repr__(self):
+        return self.direction
+
+
+NORTH = CardinalDirection("NORTH")
+WEST = CardinalDirection("WEST")
+SOUTH = CardinalDirection("SOUTH")
+EAST = CardinalDirection("EAST")
 EMPTY = "Empty"
 
 
 @dataclass
 class Plane:
-    direction: str
+    direction: CardinalDirection
 
 
 NORTH_FACING_PLANE = Plane(NORTH)
@@ -120,35 +62,42 @@ WEST_FACING_PLANE = Plane(WEST)
 SOUTH_FACING_PLANE = Plane(SOUTH)
 EAST_FACING_PLANE = Plane(EAST)
 
+
 class Segment:
-    def __init__(self, direction: str, length: int):
+    def __init__(self, direction: CardinalDirection, length: int):
         self.direction = direction
         self.length = length
 
-    def locations(self, starting_location):
+    def locations(self, starting_location) -> list[tuple[int, int]]:
         match self.direction:
-            case CompassDirection(direction="NORTH"):
-                return list(zip(range(starting_location[0], starting_location[0] - self.length, -1), itertools.repeat(starting_location[1], self.length)))
-            case CompassDirection(direction="WEST"):
-                return list(zip(itertools.repeat(starting_location[0], self.length), range(starting_location[1], starting_location[1] + self.length)))
-            case CompassDirection(direction="SOUTH"):
-                return list(zip(range(starting_location[0], starting_location[0] + self.length), itertools.repeat(starting_location[1], self.length)))
-            case CompassDirection(direction="EAST"):
-                return list(zip(itertools.repeat(starting_location[0], self.length), range(starting_location[1], starting_location[1] - self.length, -1)))
+            case CardinalDirection(direction="NORTH"):
+                return list(zip(range(starting_location[0], starting_location[0] - self.length, -1),
+                                itertools.repeat(starting_location[1], self.length)))
+            case CardinalDirection(direction="WEST"):
+                return list(zip(itertools.repeat(starting_location[0], self.length),
+                                range(starting_location[1], starting_location[1] + self.length)))
+            case CardinalDirection(direction="SOUTH"):
+                return list(zip(range(starting_location[0], starting_location[0] + self.length),
+                                itertools.repeat(starting_location[1], self.length)))
+            case CardinalDirection(direction="EAST"):
+                return list(zip(itertools.repeat(starting_location[0], self.length),
+                                range(starting_location[1], starting_location[1] - self.length, -1)))
 
-    def displacement(self):
+    def displacement(self) -> np.ndarray[int]:
         match self.direction:
-            case CompassDirection(direction="NORTH"):
+            case CardinalDirection(direction="NORTH"):
                 return np.array((-self.length, 0))
-            case CompassDirection(direction="WEST"):
+            case CardinalDirection(direction="WEST"):
                 return np.array((0, self.length))
-            case CompassDirection(direction="SOUTH"):
+            case CardinalDirection(direction="SOUTH"):
                 return np.array((self.length, 0))
-            case CompassDirection(direction="EAST"):
+            case CardinalDirection(direction="EAST"):
                 return np.array((0, -self.length))
 
     @classmethod
     def from_points(cls, point1, point2):
+        if point1[0] != point2[0] and point1[1] != point2[1]:
+            raise ValueError("Given points do not lie along a cardinal direction.")
         if point1[0] < point2[0]:
             return Segment(SOUTH, point2[0] - point1[0])
         elif point1[1] < point2[1]:
@@ -157,10 +106,11 @@ class Segment:
             return Segment(EAST, point1[1] - point2[1])
         elif point1[0] > point2[0]:
             return Segment(NORTH, point1[0] - point2[0])
-        
+        else: # point1 == point2:
+            raise ValueError("Given points create a segment of length 0. It is not possible to infer the direction.")
 
 @dataclass
-class Filling:
+class PathFilling:
     filling: list
 
     def enumerate_just_the_planes(self):
@@ -169,95 +119,115 @@ class Filling:
                 continue
             yield i, content
 
-    def __getitem__(self, index):
-        return self.filling[index]
+    def __getitem__(self, item):
+        return self.filling[item]
+
+Point = np.array
+
 class Path:
-    FORWARD = "FORWARD"
-    BACKWARD = "BACKWARD"
-    OUT = "OUT"
-    
-    def __init__(self, start: np.array, segments: list[Segment], flying_forward_mandatory: bool, mandatory_planes:list[int]):
+    def __init__(self, start: Point, segments: list[Segment]):
         """
-        Creates that goes from point to point in straight vertical or horizontal lines
+        Creates that goes from point to point in straight vertical or horizontal segments
         """
         self.corners = []
         self.directions = []
         self.locations = []
+        self.segments = segments
         current_location = start
         for segment in segments:
-            self.directions += [segment.direction]*segment.length
+            self.directions += [segment.direction] * segment.length
             self.locations += segment.locations(current_location)
             current_location += segment.displacement()
             self.corners.append(tuple(current_location))
-        
+
         self.corners.pop()
         self.directions.append(self.directions[-1])
         self.locations.append(tuple(current_location))
-        
-        self.flying_forward_mandatory = flying_forward_mandatory
-        self.mandatory_planes = mandatory_planes
-        
+
     @classmethod
-    def from_points(cls, points, flying_forward_mandatory: bool=False, mandatory_planes:list[int]=[]):
+    def from_points(cls, points: tuple[tuple[int, int], ...]):
         points = list(map(np.array, points))
         start = points[0]
         segments = []
-        for i in range(len(points)-1):
-            segments.append(Segment.from_points(points[i], points[i+1]))
-        return cls(start, segments, flying_forward_mandatory, mandatory_planes)
+        for i in range(len(points) - 1):
+            segments.append(Segment.from_points(points[i], points[i + 1]))
+        return cls(start, segments)
 
-        
-    def verify_filling(self, filling: Filling):
+
+class PathObjective(Path):
+    FORWARD = "FORWARD"
+    BACKWARD = "BACKWARD"
+    OUT = "OUT"
+
+    def __init__(self, start: Point, segments: list[Segment], flying_forward_mandatory: bool,
+                 mandatory_planes: tuple[int]):
+        """
+        Creates that goes from point to point in straight vertical or horizontal lines
+        """
+        super().__init__(start, segments)
+
+        self.flying_forward_mandatory = flying_forward_mandatory
+        self.mandatory_planes = mandatory_planes
+
+    @classmethod
+    def from_points(cls, points: tuple[tuple[int, int], ...], flying_forward_mandatory: bool=False,
+                    mandatory_planes: tuple[int]=()):
+        path = Path.from_points(points)
+        return cls(path.locations[0], path.segments, flying_forward_mandatory, mandatory_planes)
+
+    def raise_exception_if_filling_invalid(self, filling: PathFilling):
         self._check_all_mandatory_planes_present(filling)
         self._check_no_planes_on_corners(filling)
         self._check_all_planes_along_path(filling)
-        
+
         if self.flying_forward_mandatory:
             self._check_planes_fly_in_forward_direction(filling)
         else:
             self._check_planes_fly_in_same_direction(filling)
 
-        return True
-
-    def _check_all_mandatory_planes_present(self, filling: Filling):
+    def _check_all_mandatory_planes_present(self, filling: PathFilling):
         for mandatory_plane in self.mandatory_planes:
             self._check_mandatory_plane_present(filling, mandatory_plane)
 
-    def _check_mandatory_plane_present(self, filling, mandatory_plane):
+    @staticmethod
+    def _check_mandatory_plane_present(filling, mandatory_plane):
         if filling[mandatory_plane] == EMPTY:
-            raise MissingPlaneError(f"Plane missing at location {mandatory_plane}")
+            raise MissingPlaneException(f"Plane missing at index {mandatory_plane}")
 
-    def _check_no_planes_on_corners(self, filling: Filling):
+    def _check_no_planes_on_corners(self, filling: PathFilling):
         for corner in self.corners:
             self._check_no_plane_on_corner(corner, filling)
 
     def _check_no_plane_on_corner(self, corner, filling):
         index = self.locations.index(corner)
         if filling[index] != EMPTY:
-            raise PlaneLocationError
+            raise PlaneLocationException
 
-    def _check_all_planes_along_path(self, filling: Filling):
-        for location, plane in filling.enumerate_just_the_planes():
-            if self._forward_backward_or_out(location, plane.direction) == self.OUT:
-                raise PlaneDirectionError(f"Plane at {location} not along path")
-    
-    def _check_planes_fly_in_forward_direction(self, filling: Filling):
-        for location, plane in filling.enumerate_just_the_planes():
-            if self._forward_backward_or_out(location, plane.direction) != self.FORWARD:
-                raise PlaneDirectionError("Plane not flying forward")
+    def _check_all_planes_along_path(self, filling: PathFilling):
+        for i, plane in filling.enumerate_just_the_planes():
+            if self._forward_backward_or_out(i, plane.direction) == self.OUT:
+                raise PlaneDirectionException(f"Plane at index {i} not along path")
 
-    def _check_planes_fly_in_same_direction(self, filling: Filling):
+    def _check_planes_fly_in_forward_direction(self, filling: PathFilling):
+        for i, plane in filling.enumerate_just_the_planes():
+            if self._forward_backward_or_out(i, plane.direction) != self.FORWARD:
+                raise PlaneDirectionException(f"Plane at index {i} not flying forward")
+
+    def _check_planes_fly_in_same_direction(self, filling: PathFilling):
         previous_direction = None
-        for location, plane in filling.enumerate_just_the_planes():
-        
+
+        for i, plane in filling.enumerate_just_the_planes():
+
             if previous_direction is None:
-                previous_direction = self._forward_backward_or_out(location, plane.direction)
+                previous_direction = self._forward_backward_or_out(i, plane.direction)
+                prev_i = i
                 continue
 
-            current_direction = self._forward_backward_or_out(location, plane.direction)
+            current_direction = self._forward_backward_or_out(i, plane.direction)
             if previous_direction != current_direction:
-                raise PlaneDirectionError("Not all planes are going in the same direction.")
+                raise PlaneDirectionException(f"Planes at indices {prev_i} and {i} are going in different directions.")
 
+            prev_i = i
             previous_direction = current_direction
 
     def _forward_backward_or_out(self, location, direction):
@@ -267,11 +237,25 @@ class Path:
             return self.BACKWARD
         else:
             return self.OUT
-            
 
-class Board:
-    def __init__(self, paths: list[Path]):
+class BoardFilling:
+    def __init__(self, filling: list[list[...]]):
+        self.filling = np.array(filling)
+    def __getitem__(self, item):
+        return self.filling[item]
+    def restrict_to_path(self, path: Path):
+        return PathFilling([self[location] for location in path.locations])
+
+
+class BoardObjective:
+    def __init__(self, paths: tuple[PathObjective, ...]):
         self.paths = paths
+
+    def raise_exception_if_filling_invalid(self, board_filling: BoardFilling):
+        for path in self.paths:
+            path_filling = board_filling.restrict_to_path(path)
+            path.raise_exception_if_filling_invalid(path_filling)
+
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -1,8 +1,177 @@
 import unittest
+from gameBoard import *
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestCardinalDirections(unittest.TestCase):
+    def test_invalid_direction(self):
+        with self.assertRaises(ValueError):
+            CardinalDirection("northwest")
+
+
+class TestSegmentMethods(unittest.TestCase):
+    def test_from_points_length(self):
+        for i, j in itertools.product(range(10), range(10)):
+            if i == j:
+                continue
+            self.assertEqual(Segment.from_points((0,i), (0,j)).length, abs(i-j))
+            self.assertEqual(Segment.from_points((i, 0), (j, 0)).length, abs(i - j))
+
+    def test_from_points_zero_length(self):
+        for i, j in itertools.product(range(10), range(10)):
+            with self.assertRaises(ValueError):
+                Segment.from_points((i,j),(i,j))
+    def test_from_points_diagonal(self):
+        for i, j in itertools.product(range(1,10), range(1,10)):
+            with self.assertRaises(ValueError):
+                Segment.from_points((0, 0), (i, j))
+    def test_from_points_north(self):
+        self.assertEqual(Segment.from_points((0,0), (-2, 0)).direction, NORTH)
+
+    def test_from_points_west(self):
+        self.assertEqual(Segment.from_points((0,0), (0, 2)).direction, WEST)
+
+    def test_from_points_south(self):
+        self.assertEqual(Segment.from_points((0,0), (2, 0)).direction, SOUTH)
+
+    def test_from_points_east(self):
+        self.assertEqual(Segment.from_points((0,0), (0, -2)).direction, EAST)
+
+
+class TestPathFillingMethods(unittest.TestCase):
+    def test_index(self):
+        filling = PathFilling([EMPTY, NORTH_FACING_PLANE, EMPTY, EMPTY, SOUTH_FACING_PLANE])
+        self.assertEqual(filling[2], EMPTY)
+        self.assertEqual(filling[1], NORTH_FACING_PLANE)
+
+    def test_enumerate(self):
+        filling = PathFilling([WEST_FACING_PLANE, EMPTY, EMPTY, EMPTY, EMPTY, SOUTH_FACING_PLANE])
+        self.assertEqual(list(filling.enumerate_just_the_planes()),
+                         [(0,WEST_FACING_PLANE), (5, SOUTH_FACING_PLANE)])
+
+        filling = PathFilling([WEST_FACING_PLANE, NORTH_FACING_PLANE, EAST_FACING_PLANE, EMPTY, EMPTY, SOUTH_FACING_PLANE])
+        self.assertEqual(list(filling.enumerate_just_the_planes()),
+                         [(0, WEST_FACING_PLANE), (1, NORTH_FACING_PLANE), (2, EAST_FACING_PLANE), (5, SOUTH_FACING_PLANE)])
+
+
+class TestPathObjectiveMethods(unittest.TestCase):
+    def test_path_from_points(self):
+        path = PathObjective.from_points(((1, 1), (3, 1), (3, 2), (1, 2)))
+        self.assertEqual(path.locations, [(1, 1), (2, 1), (3, 1), (3, 2), (2, 2), (1, 2)])
+
+    def test_good_path_gets_accepted(self):
+        path = PathObjective.from_points(((1, 1), (3, 1), (3, 2), (1, 2)))
+        filling = PathFilling([EMPTY, NORTH_FACING_PLANE, EMPTY, EMPTY, SOUTH_FACING_PLANE])
+        self.assertIsNone(path.raise_exception_if_filling_invalid(filling))
+
+    def test_plane_not_along_path(self):
+        path = PathObjective.from_points(((1, 1), (3, 1), (3, 2)))
+        filling = PathFilling([EMPTY, NORTH_FACING_PLANE, EMPTY, NORTH_FACING_PLANE])
+        with self.assertRaises(PlaneDirectionException):
+            path.raise_exception_if_filling_invalid(filling)
+
+    def test_planes_in_different_directions(self):
+        path = PathObjective.from_points(((1, 1), (3, 1), (3, 2)))
+        filling = PathFilling([EMPTY, NORTH_FACING_PLANE, EMPTY, WEST_FACING_PLANE])
+        with self.assertRaises(PlaneDirectionException):
+            path.raise_exception_if_filling_invalid(filling)
+
+    def test_plane_on_corner(self):
+        path = PathObjective.from_points(((1, 1), (3, 1), (3, 2)))
+        filling = PathFilling([EMPTY, NORTH_FACING_PLANE, NORTH_FACING_PLANE, EMPTY])
+        with self.assertRaises(PlaneLocationException):
+            path.raise_exception_if_filling_invalid(filling)
+
+    def test_plane_at_start_and_end(self):
+        path = PathObjective.from_points(((1, 1), (1, 4), (3, 4)))
+        filling = PathFilling([WEST_FACING_PLANE, EMPTY, EMPTY, EMPTY, EMPTY, SOUTH_FACING_PLANE])
+        self.assertIsNone(path.raise_exception_if_filling_invalid(filling))
+
+    def test_bad_plane_at_end(self):
+        path = PathObjective.from_points(((1, 1), (1, 4), (3, 4)))
+        filling = PathFilling([EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, WEST_FACING_PLANE])
+        with self.assertRaises(PlaneDirectionException):
+            path.raise_exception_if_filling_invalid(filling)
+
+    def test_bad_plane_at_start(self):
+        path = PathObjective.from_points(((1, 1), (1, 4), (3, 4)))
+        filling = PathFilling([SOUTH_FACING_PLANE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY])
+        with self.assertRaises(PlaneDirectionException):
+            path.raise_exception_if_filling_invalid(filling)
+
+    def test_directed_path_followed(self):
+        path = PathObjective.from_points(((1, 1), (1, 4), (3, 4)), flying_forward_mandatory=True)
+        filling = PathFilling([WEST_FACING_PLANE, EMPTY, EMPTY, EMPTY, EMPTY, SOUTH_FACING_PLANE])
+        self.assertIsNone(path.raise_exception_if_filling_invalid(filling))
+
+    def test_directed_path_not_followed(self):
+        path = PathObjective.from_points(((3, 4), (1, 4), (1, 1)), flying_forward_mandatory=True)
+        filling = PathFilling([WEST_FACING_PLANE, EMPTY, EMPTY, EMPTY, EMPTY, SOUTH_FACING_PLANE])
+        with self.assertRaises(PlaneDirectionException):
+            path.raise_exception_if_filling_invalid(filling)
+
+    def test_mandatory_planes_present(self):
+        path = PathObjective.from_points(((50, 43), (50, 50)), mandatory_planes=(0, 1, 2))
+        filling = PathFilling(
+            [WEST_FACING_PLANE, WEST_FACING_PLANE, WEST_FACING_PLANE, EMPTY, EMPTY, WEST_FACING_PLANE, EMPTY, EMPTY])
+        self.assertIsNone(path.raise_exception_if_filling_invalid(filling))
+
+    def test_mandatory_plane_missing(self):
+        path = PathObjective.from_points(((50, 43), (50, 50)), mandatory_planes=(0, 1, 2))
+        filling = PathFilling([WEST_FACING_PLANE, EMPTY, WEST_FACING_PLANE, EMPTY, EMPTY, WEST_FACING_PLANE, EMPTY, EMPTY])
+        with self.assertRaises(MissingPlaneException):
+            path.raise_exception_if_filling_invalid(filling)
+
+
+class TestBoardFillingMethods(unittest.TestCase):
+    def test_indexing(self):
+        filling = BoardFilling([[EMPTY, WEST_FACING_PLANE, EMPTY, SOUTH_FACING_PLANE],
+                                [WEST_FACING_PLANE, EMPTY, NORTH_FACING_PLANE, EMPTY]])
+
+        self.assertEqual(filling[1, 1], EMPTY)
+        self.assertEqual(filling[1, 3], EMPTY)
+        self.assertEqual(filling[0, 1], WEST_FACING_PLANE)
+
+    def test_restriction_to_path(self):
+        filling = BoardFilling([[WEST_FACING_PLANE, WEST_FACING_PLANE, EMPTY, EMPTY],
+                                [EMPTY, WEST_FACING_PLANE, EMPTY, EMPTY],
+                                [EMPTY, EMPTY, EMPTY, SOUTH_FACING_PLANE],
+                                [EMPTY, EMPTY, EMPTY, EMPTY]])
+        path = Path.from_points(((0,0), (0,3)))
+        print(filling.restrict_to_path(path))
+        self.assertEqual(filling.restrict_to_path(path), PathFilling([WEST_FACING_PLANE, WEST_FACING_PLANE, EMPTY, EMPTY]))
+
+
+class TestBoardObjectiveMethods(unittest.TestCase):
+    def test_good_filling_passes(self):
+        board = BoardObjective((PathObjective.from_points(((3, 3), (1, 3), (1, 1))),PathObjective.from_points(((0,0), (0, 3)))))
+        filling = BoardFilling([[WEST_FACING_PLANE, WEST_FACING_PLANE, EMPTY, EMPTY],
+                                [EMPTY, WEST_FACING_PLANE, EMPTY, EMPTY],
+                                [EMPTY, EMPTY, EMPTY, SOUTH_FACING_PLANE],
+                                [EMPTY, EMPTY, EMPTY, EMPTY]])
+        self.assertIsNone(board.raise_exception_if_filling_invalid(filling))
+
+    def test_crossing_paths(self):
+        board = BoardObjective((PathObjective.from_points(((0, 1), (2,1))),PathObjective.from_points(((1, 0), (1,2)))))
+        filling = BoardFilling([[EMPTY, EMPTY, EMPTY],
+                               [EMPTY, EMPTY, EMPTY],
+                               [EMPTY, EMPTY, EMPTY]])
+
+        self.assertIsNone(board.raise_exception_if_filling_invalid(filling))
+
+        filling = BoardFilling([[EMPTY, EMPTY, EMPTY],
+                                [EMPTY, WEST_FACING_PLANE, EMPTY],
+                                [EMPTY, EMPTY, EMPTY]])
+        with self.assertRaises(PlaneDirectionException):
+            board.raise_exception_if_filling_invalid(filling)
+
+    def test_parallel_overlapping_paths(self):
+        board = BoardObjective(
+            (PathObjective.from_points(((0, 1), (2, 1))), PathObjective.from_points(((0, 0), (0, 1), (2, 1), (2, 0)))))
+        filling = BoardFilling([[EMPTY, EMPTY, EMPTY],
+                                [EMPTY, SOUTH_FACING_PLANE, EMPTY],
+                                [EMPTY, EMPTY, EMPTY]])
+
+        self.assertIsNone(board.raise_exception_if_filling_invalid(filling))
+
 
 if __name__ == '__main__':
     unittest.main()
