ff9d606e88feb6546cd30bd0af4d69c435f16f80
MartinVanIJcken <daanvanijcken@hotmail.com>
2024-06-28 16:53:21 +0200

    Started work on levels, they will be a pair of an objective and tiles and will require exactly the tiles given to be used for creating the tiling (and filling)

---
+++ level.py
@@ -0,0 +1,11 @@
+from board import BoardObjective
+from tiling import Tile
+from collections import Counter
+class Level:
+    def __init__(self, objective: BoardObjective, tiles: list[Tile]):
+        self.objective = objective
+        self.tiles = tiles
+
+    def raise_exception_if_tiling_invalid(self, tiling):
+        if Counter(self.tiles) != Counter(tiling.tiles):
+            raise ValueError("These are not the right tiles")
--- levelTests.py
+++ levelTests.py
@@ -5,9 +5,8 @@ from tileComponents import UNCOVERED, COVERED, NORTH_FACING_PLANE, WEST_FACING_P
 from board import BoardObjective, PathObjective
 
 class TestLevelMethods(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
+    def test_invalid_tiles(self):
+        self.DEFAULT_TILE_1
 
 if __name__ == '__main__':
     unittest.main()
