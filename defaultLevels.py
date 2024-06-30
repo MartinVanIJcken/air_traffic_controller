3702fb823a1c59885ce207164d7c11e5fb275b15
MartinVanIJcken <daanvanijcken@hotmail.com>
2024-06-30 22:17:44 +0200

    Made my editor happy by not using weird hacks to create variables in a way it doesn't recognize

--- defaultLevels.py
+++ defaultLevels.py
@@ -3,7 +3,7 @@ import itertools
 from tiling import Tile
 from tileComponents import *
 from level import Level
-from board import BoardObjective, PathObjective, Segment
+from board import BoardObjective, PathObjective, Segment, Point
 
 DEFAULT_TILE_1 = Tile([[COVERED], [NORTH_FACING_PLANE]])  # orange
 DEFAULT_TILE_2 = Tile([[COVERED, NORTH_FACING_PLANE]])  # red
@@ -14,9 +14,22 @@ DEFAULT_TILE_6 = Tile([[COVERED, UNCOVERED], [COVERED, NORTH_FACING_PLANE]])  # 
 
 DEFAULT_TILES = [DEFAULT_TILE_1, DEFAULT_TILE_2, DEFAULT_TILE_3, DEFAULT_TILE_4, DEFAULT_TILE_5, DEFAULT_TILE_6]
 
-# a hack to get a shorthand for all points
-for i, j in itertools.product(range(4), range(4)):
-    globals()["p"+i+j] = Point((i,j))
+p00 = Point((0, 0))
+p01 = Point((0, 1))
+p02 = Point((0, 2))
+p03 = Point((0, 3))
+p10 = Point((1, 0))
+p11 = Point((1, 1))
+p12 = Point((1, 2))
+p13 = Point((1, 3))
+p20 = Point((2, 0))
+p21 = Point((2, 1))
+p22 = Point((2, 2))
+p23 = Point((2, 3))
+p30 = Point((3, 0))
+p31 = Point((3, 1))
+p32 = Point((3, 2))
+p33 = Point((3, 3))
 
 level7 = Level(objective=BoardObjective([PathObjective(p00, [Segment(SOUTH, 1)], mandatory_planes=(0,)),
                                          PathObjective(p03, [Segment(WEST, 1)], mandatory_planes=(0,)),
