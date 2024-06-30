8a50e286c5e07e3391bc871a21d57b63053c61eb
MartinVanIJcken <daanvanijcken@hotmail.com>
2024-06-30 22:34:50 +0200

    reformatted

--- levelTests.py
+++ levelTests.py
@@ -1,16 +1,14 @@
 import unittest
 
-from defaultLevels import *
+from defaultLevels import level7, DEFAULT_TILE_1, DEFAULT_TILE_2, \
+    DEFAULT_TILE_3, DEFAULT_TILE_4, DEFAULT_TILE_5, DEFAULT_TILE_6
 from tiling import Tiling
 from errorsAndExceptions import *
 
 class TestLevelMethods(unittest.TestCase):
-    LEVEL_7 = level7
-
     def test_level_7_correct(self):
-        level = self.LEVEL_7
-
-        correct_tiling = Tiling([(0,0), (0,1), (0,2), (2,0), (2,1), (2,3)],
+        level = level7
+        correct_tiling = Tiling([(0, 0), (0, 1), (0, 2), (2, 0), (2, 1), (2, 3)],
                                 [DEFAULT_TILE_1.rotation(2), DEFAULT_TILE_6, DEFAULT_TILE_4.rotation(1),
                                  DEFAULT_TILE_5.rotation(1), DEFAULT_TILE_3.rotation(2), DEFAULT_TILE_2.rotation(1)], shape=(4,4))
 
@@ -18,8 +16,7 @@ class TestLevelMethods(unittest.TestCase):
 
 
     def test_level_7_wrong_tiling(self):
-        level = self.LEVEL_7
-
+        level = level7
         wrong_tiling = Tiling([(0, 0), (0, 1), (0, 3), (2, 0), (2, 1), (2, 2)],
                               [DEFAULT_TILE_5.rotation(1), DEFAULT_TILE_3.rotation(2), DEFAULT_TILE_2.rotation(1),
                                DEFAULT_TILE_1.rotation(2), DEFAULT_TILE_6, DEFAULT_TILE_4.rotation(1)],
@@ -29,7 +26,7 @@ class TestLevelMethods(unittest.TestCase):
             level.raise_exception_if_tiling_invalid(wrong_tiling)
 
     def test_level_7_wrong_tiles(self):
-        level = self.LEVEL_7
+        level = level7
         wrong_tiles_tiling = Tiling([(0,0), (1,1), (0,2), (2,0), (2,1), (2,3)],
                                 [DEFAULT_TILE_4.rotation(2), DEFAULT_TILE_2, DEFAULT_TILE_4.rotation(1),
                                  DEFAULT_TILE_5.rotation(1), DEFAULT_TILE_3.rotation(2), DEFAULT_TILE_2.rotation(1)], shape=(4,4))
